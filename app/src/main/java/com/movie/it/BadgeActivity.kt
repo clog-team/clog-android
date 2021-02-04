2282ba8364ee5565c2e838b0dd600691b9888847
sys09270883 <50787869+sys09270883@users.noreply.github.com>
2021-02-04 22:49:41 +0900

    Create activites

---
+++ app/src/main/java/com/movie/it/BadgeActivity.kt
@@ -0,0 +1,11 @@
+package com.movie.it
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BadgeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_badge)
+    }
+}
---
+++ app/src/main/java/com/movie/it/WishListActivity.kt
@@ -0,0 +1,11 @@
+package com.movie.it
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WishListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wish_list)
+    }
+}
